bf4f2392681db4f9a71586b482dd86894193d848
mojonny <moirjonathan@gmail.com>
2023-01-15 13:03:39 -0700

    switched back to chrome and fixed issue

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -28,6 +28,7 @@ googleProvider.setCustomParameters({
 export const auth = getAuth();
 export const signInWithGooglePopup = () =>
 	signInWithPopup(auth, googleProvider);
+
 export const signInWithGoogleRedirect = () =>
 	signInWithRedirect(auth, googleProvider);
 

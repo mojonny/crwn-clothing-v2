a13c25fa776b30a12c88cbbe5fc16bfbf7324014
mojonny <moirjonathan@gmail.com>
2023-01-15 07:44:45 -0700

    Learning sass and components

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,12 @@
+import "./directory.styles.scss";
+import CategoryItem from "../category-item/category-item.component";
+
+export default function Directory({ categories }) {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+}
